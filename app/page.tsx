a04781a230e6ffc4c39c380e387803e7a2189215
Ayushatre05 <ayushkatre1801@gmail.com>
2024-06-28 20:48:11 +0530

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Head from 'next/head';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
