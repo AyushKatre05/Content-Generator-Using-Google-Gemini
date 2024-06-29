d858d2c4fa1f64b3d0f6017624a7e6a34cb0f75f
Ayushatre05 <ayushkatre1801@gmail.com>
2024-06-29 10:03:32 +0530

    done

--- app/dashboard/(components)/SearchSection.tsx
+++ app/dashboard/(components)/SearchSection.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import { Home, Search } from "lucide-react";
 import Link from "next/link";
 import React from "react";
@@ -7,12 +8,11 @@ const SearchSection = ({ onSearchInput }: any) => {
     <div className="p-6 md:p-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col md:flex-row justify-between items-center text-white">
       <div className="flex items-center gap-4">
         <Link href="/">
-        <h2 className="text-2xl md:text-3xl font-bold">Content Generator</h2>
+          <h2 className="text-2xl md:text-3xl font-bold cursor-pointer">Content Generator</h2>
         </Link>
-        
       </div>
-      <div className="w-full md:w-[30%] flex items-center justify-center mt-4 md:mt-0">
-        <div className="w-full max-w-md flex gap-2 items-center p-2 border rounded-md bg-orange-50">
+      <div className="flex justify-center items-center mt-4 md:mt-0 space-x-4 md:space-x-6 w-full max-w-xl">
+        <div className="w-full flex items-center p-2 border rounded-md bg-orange-50">
           <Search className="text-primary" />
           <input
             placeholder="Search"
@@ -21,6 +21,7 @@ const SearchSection = ({ onSearchInput }: any) => {
             className="bg-transparent w-full outline-none text-black placeholder-gray-500"
           />
         </div>
+        <UserButton />
       </div>
     </div>
   );
